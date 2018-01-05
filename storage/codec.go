edce3b4acc995696d8b682f8acd9bbc9f00734d3
Markus Stenberg <markus.stenberg@iki.fi>
2018-01-06 01:25:05 +0200

    storage: Fixed inmemorybackend + codecBackend interaction.

--- storage/codec.go
+++ storage/codec.go
@@ -4,8 +4,8 @@
  * Copyright (c) 2018 Markus Stenberg
  *
  * Created:       Sat Jan  6 00:13:13 2018 mstenber
- * Last modified: Sat Jan  6 00:20:43 2018 mstenber
- * Edit time:     4 min
+ * Last modified: Sat Jan  6 01:22:50 2018 mstenber
+ * Edit time:     5 min
  *
  */
 
@@ -26,6 +26,16 @@ func (self codecBackend) Init(backend Backend, codec codec.Codec) *codecBackend 
 	self.backend = backend
 	self.codec = codec
 	return &self
+}
+
+func (self *codecBackend) GetBlockById(id string) *Block {
+	b := self.backend.GetBlockById(id)
+	if b == nil || b.Data == nil {
+		return b
+	}
+	nb := *b
+	nb.Data = nil
+	return &nb
 }
 
 func (self *codecBackend) GetBlockData(bl *Block) []byte {
--- storage/inmemory/inmemory.go
+++ storage/inmemory/inmemory.go
@@ -4,8 +4,8 @@
  * Copyright (c) 2017 Markus Stenberg
  *
  * Created:       Sun Dec 17 22:20:08 2017 mstenber
- * Last modified: Fri Jan  5 12:44:30 2018 mstenber
- * Edit time:     63 min
+ * Last modified: Sat Jan  6 01:21:09 2018 mstenber
+ * Edit time:     71 min
  *
  */
 
@@ -22,7 +22,7 @@ import (
 // inMemoryBackend provides In-memory storage; data is always
 // assumed to be available and is just stored in maps.
 type inMemoryBackend struct {
-	id2Block map[string]*storage.Block
+	id2Block map[string]storage.Block
 	name2Id  map[string]string
 	lock     util.MutexLocked
 }
@@ -32,7 +32,7 @@ var _ storage.Backend = &inMemoryBackend{}
 // Init makes the instance actually useful
 func NewInMemoryBackend() storage.Backend {
 	self := &inMemoryBackend{}
-	self.id2Block = make(map[string]*storage.Block)
+	self.id2Block = make(map[string]storage.Block)
 	self.name2Id = make(map[string]string)
 	return self
 }
@@ -46,13 +46,21 @@ func (self *inMemoryBackend) DeleteBlock(b *storage.Block) {
 	delete(self.id2Block, b.Id)
 }
 
-func (self *inMemoryBackend) GetBlockData(b *storage.Block) []byte {
+func (self *inMemoryBackend) GetBlockData(bl *storage.Block) []byte {
+	b, ok := self.id2Block[bl.Id]
+	if !ok {
+		log.Fatal("Non-existent block id in GetBlockData")
+	}
 	return b.Data
 }
 
 func (self *inMemoryBackend) GetBlockById(id string) *storage.Block {
 	defer self.lock.Locked()()
-	return self.id2Block[id]
+	b, ok := self.id2Block[id]
+	if !ok {
+		return nil
+	}
+	return &b
 }
 
 func (self *inMemoryBackend) GetBlockIdByName(name string) string {
@@ -75,16 +83,18 @@ func (self *inMemoryBackend) SetNameToBlockId(name, block_id string) {
 
 func (self *inMemoryBackend) StoreBlock(b *storage.Block) {
 	defer self.lock.Locked()()
-	if self.id2Block[b.Id] != nil {
+	_, ok := self.id2Block[b.Id]
+	if ok {
 		log.Fatal("Existing block id in StoreBlock")
 	}
 	mlog.Printf2("storage/inmemory/inmemory", "im.StoreBlock %x", b.Id)
-	self.id2Block[b.Id] = b
+	self.id2Block[b.Id] = *b
 }
 
 func (self *inMemoryBackend) UpdateBlock(b *storage.Block) int {
 	defer self.lock.Locked()()
-	if self.id2Block[b.Id] == nil {
+	_, ok := self.id2Block[b.Id]
+	if !ok {
 		log.Fatal("Non-existent block id in StoreBlock")
 	}
 	mlog.Printf2("storage/inmemory/inmemory", "im.UpdateBlock %x", b.Id)

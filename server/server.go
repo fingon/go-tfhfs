0062a2856555211ff64d4cec9f05ed4620a72e37
Markus Stenberg <markus.stenberg@iki.fi>
2018-01-17 17:15:11 +0200

    server: Shortcircuit GetBlockIdByName if it is actually current storage root name.

--- server/server.go
+++ server/server.go
@@ -4,8 +4,8 @@
  * Copyright (c) 2018 Markus Stenberg
  *
  * Created:       Tue Jan 16 14:38:35 2018 mstenber
- * Last modified: Wed Jan 17 16:48:11 2018 mstenber
- * Edit time:     132 min
+ * Last modified: Wed Jan 17 17:14:40 2018 mstenber
+ * Edit time:     135 min
  *
  */
 
@@ -61,6 +61,13 @@ func (self *Server) ClearBlocksInName(ctx context.Context, n *BlockName) (*Clear
 }
 
 func (self *Server) GetBlockIdByName(ctx context.Context, name *BlockName) (*BlockId, error) {
+	if name.Name == self.Fs.RootName {
+		block := self.Fs.RootBlock()
+		if block != nil {
+			return &BlockId{Id: block.Id()}, nil
+
+		}
+	}
 	id := self.Storage.GetBlockIdByName(name.Name)
 	return &BlockId{Id: id}, nil
 }

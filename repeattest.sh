70a741d6275a4eb069080957f70949c9dcdd9ff2
Markus Stenberg <markus.stenberg@iki.fi>
2018-03-13 10:42:03 +0200

    repeattest: Added -count 1 to prevent Go 1.10's test result caching from fucking it up.

--- repeattest.sh
+++ repeattest.sh
@@ -6,7 +6,7 @@
 # Copyright (c) 2018 Markus Stenberg
 #
 # Created:       Mon Jan  8 12:57:07 2018 mstenber
-# Last modified: Tue Jan  9 19:01:17 2018 mstenber
+# Last modified: Tue Mar 13 10:29:48 2018 mstenber
 # Edit time:     10 min
 #
 
@@ -21,7 +21,7 @@ rm ,log* || true
 for ITER in `seq 1 100`
 do
     echo -n Iteration $ITER ..
-    if go test $* >& ,log$ITER
+    if go test -count 1 $* >& ,log$ITER
     then
         echo "+"
         rm -f ,log$ITER

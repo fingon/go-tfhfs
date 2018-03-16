daf47e9b8326a48eee0b1c5b5e84b6178497943b
Markus Stenberg <markus.stenberg@iki.fi>
2018-03-16 12:55:28 +0200

    util: Added IMin/IMax.

--- util/util.go
+++ util/util.go
@@ -4,8 +4,8 @@
  * Copyright (c) 2017 Markus Stenberg
  *
  * Created:       Fri Dec 29 09:03:12 2017 mstenber
- * Last modified: Fri Dec 29 09:08:59 2017 mstenber
- * Edit time:     3 min
+ * Last modified: Fri Mar 16 11:40:20 2018 mstenber
+ * Edit time:     4 min
  *
  */
 
@@ -35,4 +35,22 @@ func Uint64Bytes(n uint64) []byte {
 	nb := make([]byte, 8)
 	binary.BigEndian.PutUint64(nb, n)
 	return nb
+}
+
+func IMin(i int, ints ...int) int {
+	for _, v := range ints {
+		if v < i {
+			i = v
+		}
+	}
+	return i
+}
+
+func IMax(i int, ints ...int) int {
+	for _, v := range ints {
+		if v > i {
+			i = v
+		}
+	}
+	return i
 }
--- util/util_test.go
+++ util/util_test.go
@@ -4,7 +4,7 @@
  * Copyright (c) 2017 Markus Stenberg
  *
  * Created:       Fri Dec 29 09:04:44 2017 mstenber
- * Last modified: Fri Dec 29 09:05:17 2017 mstenber
+ * Last modified: Fri Mar 16 11:41:03 2018 mstenber
  * Edit time:     0 min
  *
  */
@@ -21,4 +21,17 @@ func TestConcatBytes(t *testing.T) {
 	t.Parallel()
 
 	assert.Equal(t, ConcatBytes([]byte("foo"), []byte("bar")), []byte("foobar"))
+}
+
+func TestIMin(t *testing.T) {
+	t.Parallel()
+	assert.Equal(t, IMin(1, 2, 3), 1)
+	assert.Equal(t, IMin(3, 2, 1), 1)
+
+}
+
+func TestIMax(t *testing.T) {
+	t.Parallel()
+	assert.Equal(t, IMax(1, 2, 3), 3)
+	assert.Equal(t, IMax(3, 2, 1), 3)
 }
